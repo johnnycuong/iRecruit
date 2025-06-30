3d23992057052f0102e24ec7f7b544c70c886390
johnnycuong <73330396+johnnycuong@users.noreply.github.com>
2025-06-30 22:21:13 +0700

    Add files via upload

---
+++ app/__init__.py
@@ -0,0 +1,19 @@
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'supersecretkey'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
+
+    # Import và đăng ký blueprint
+    from app.routes.auth import auth_bp
+    app.register_blueprint(auth_bp, url_prefix="/api")
+
+    return app
